51cf4460c9b0d96cb0e16683ea1e3afe147fbd19
Runas8128 <runas8128@naver.com>
2023-06-26 08:18:13 +0900

    build: add build script and ignore output file

---
+++ src/build.ts
@@ -0,0 +1,49 @@
+import { readFileSync, readdirSync, writeFileSync, copyFileSync, mkdirSync, rmSync } from "fs";
+import path, { join } from "path";
+import JSZip from "jszip";
+
+function add_zip(zip: JSZip, path_: string) {
+  console.log(`zipping ${path_}`);
+  const dir_list = readdirSync(path_, { withFileTypes: true });
+
+  dir_list
+    .filter(dir => dir.isFile())
+    .forEach(dir => {
+      const file = path.join(path_, dir.name);
+      zip.file(dir.name, readFileSync(file));
+    });
+
+  dir_list
+    .filter(dir => dir.isDirectory())
+    .forEach(dir => {
+      const folder = zip.folder(dir.name);
+      if (folder) add_zip(folder, path.join(path_, dir.name))
+    });
+
+  return zip;
+}
+
+function copyDir(...paths: string[][]) {
+  paths.forEach(path => {
+    mkdirSync(join(__dirname, '..', 'out', ...path));
+    const dir = readdirSync(join(__dirname, ...path), { withFileTypes: true });
+    dir.filter(elem => elem.isFile())
+      .forEach(file => copyFileSync(join(__dirname, ...path, file.name), join(__dirname, '..', 'out', ...path, file.name)));
+    dir.filter(elem => elem.isDirectory())
+      .forEach(directory => copyDir([...path, directory.name]));
+  });
+}
+
+copyDir(
+  ['config', 'env'],
+  ['web', 'static'],
+  ['web', 'views'],
+);
+
+copyFileSync(join(__dirname, '..', 'package.json'), join(__dirname, '..', 'out', 'package.json'));
+
+add_zip(new JSZip(), join(__dirname, '..', 'out'))
+  .generateAsync({ type: 'nodebuffer', compression: 'DEFLATE' })
+  .then(content => { writeFileSync('Ringon.zip', content); });
+
+rmSync(join('..', 'out'), { recursive: true, force: true });
