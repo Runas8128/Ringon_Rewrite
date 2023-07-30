862a2cd1eb4f82b4d61d0cb4a5888a4473f77cbe
Runas8128 <runas8128@naver.com>
2023-07-30 14:02:42 +0900

    feat: remove old deck upload trigger

--- src/bot/event/eventList.ts
+++ src/bot/event/eventList.ts
@@ -2,8 +2,6 @@ import { Event } from "./Event";
 import ready from "./ready";
 import btnClick from "./btnClick";
 import detect from "./detect";
-import detectDeckImg from "./detectDeckImg";
-import uploadDeck from "./uploadDeck";
 import memberLeft from "./memberLeft";
 
 export const eventList: Event[] = [
