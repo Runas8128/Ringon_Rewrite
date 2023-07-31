f1370794cea171257ab61c673f004a8110205c9e
Runas8128 <runas8128@naver.com>
2023-07-31 16:15:41 +0900

    build: use `--test` argument to indicate testing

--- src/config/options/client_options.ts
+++ src/config/options/client_options.ts
@@ -14,5 +14,5 @@ export const client_options = {
   ],
 }
 
-export const isTesting : boolean = false;
+export const isTesting = process.argv[2] === '--test';
 export const select = <T>(prod: T, dev: T) => isTesting ? dev : prod;
