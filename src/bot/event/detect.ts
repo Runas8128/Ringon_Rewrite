d02156e778f9d89e5aee034ad39ade8d22413786
Runas8128 <runas8128@naver.com>
2023-11-20 11:13:52 +0900

    refactor: move select_weight function to misc.ts

--- src/bot/event/detect.ts
+++ src/bot/event/detect.ts
@@ -2,6 +2,7 @@ import { Events, Message } from "discord.js";
 
 import { Event } from "./Event";
 import { MongoDB } from "../../util/mongodb";
+import { select_weight } from "../../util/misc";
 
 export default {
   name: Events.MessageCreate,
@@ -16,20 +17,3 @@ export default {
     if (b) return await channel.send(select_weight(b.map(r => r.result), b.map(r => r.ratio)));
   },
 } as Event;
-
-// TODO: move this to util.ts
-function select_weight<T>(targets: Array<T>, weights: Array<number>): T {
-  const total_weight = weights.reduce((prev, curr) => prev + curr, 0);
-  const weighted_random = Math.random() * total_weight;
-  const last_index = targets.length - 1;
-  let current_weight = 0;
-
-  for (let i = 0; i < last_index; ++i) {
-    current_weight += weights[i];
-    if (weighted_random < current_weight) {
-      return targets[i];
-    }
-  }
-
-  return targets[last_index];
-}
