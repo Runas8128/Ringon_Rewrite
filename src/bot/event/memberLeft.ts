a0b1770c59ae719c293ff40ee974c09709f70d93
Runas8128 <runas8128@naver.com>
2023-11-25 23:26:37 +0900

    chore: remove unnecessary logging message

--- src/bot/event/memberLeft.ts
+++ src/bot/event/memberLeft.ts
@@ -2,17 +2,12 @@ import { Events, GuildMember, PartialGuildMember, TextChannel } from "discord.js
 
 import { Event } from "./Event";
 import { channel } from "../../config/options/discord";
-import { loggerGen } from "../../logger";
-
-const logger = loggerGen.getLogger(__filename);
 
 export default {
   name: Events.GuildMemberRemove,
   once: false,
   async execute(member: GuildMember | PartialGuildMember) {
-    logger.info(`${member.nickname ?? member.user.username} left`);
     const adminCh = await member.guild.channels.fetch(channel.admin) as TextChannel;
-    logger.info(`adminCh: ${adminCh.name}`);
     await adminCh.send(`${member.nickname ?? member.user.username}님이 서버를 나가셨습니다 :(`);
   },
 } as Event;
